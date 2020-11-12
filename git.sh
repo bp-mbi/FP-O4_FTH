26cc0a3a896bd8f8aa88d5db50a82924f82d0f7d
Studierende <stduierende@mbi-berlin.de>
2020-11-12 16:06:08 +0000

    neue Simulationsdaten

--- git.sh
+++ git.sh
@@ -4,6 +4,7 @@ git config --global user.email "stduierende@mbi-berlin.de"
 git config --global user.name "Studierende"
 git config --global credential.helper cache
 
+git checkout Test
 git add *
 git commit -m 'neue Simulationsdaten'
 git push
